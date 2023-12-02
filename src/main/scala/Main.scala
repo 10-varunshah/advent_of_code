ce490215903b6f14be9dd248a419920b823cb62a
10.varunshah <10.varunshah@gmail.com>
2023-12-02 20:32:42 +0530

    Add Execution Code for 2023 in Main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,11 @@
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    executeAdvent2023
+  }
+
+  private def executeAdvent2023(): Unit = {
+    import advent_2023._
+
+    Day1("day1/ps1_input.txt")
   }
 }
